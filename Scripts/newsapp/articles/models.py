94caca06fa33a6f2fee6e9f61f2c7a13b53dc2c5
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2021-12-15 15:38:29 -0700

    slight changes to Article, set up admin info for myself

--- Scripts/newsapp/articles/models.py
+++ Scripts/newsapp/articles/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    article_id = models.AutoField(primary_key=True)
-    article_name = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
     no_of_pages = models.IntegerField()
     body = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+# Topic
+
+# News Source
