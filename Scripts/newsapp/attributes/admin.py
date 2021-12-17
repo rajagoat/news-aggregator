121c2f5e254c872328def8caa16863b9ed1caf7b
noelfranthomas <noelfranthomas@gmail.com>
2021-12-16 20:59:29 -0700

    Added some here

--- Scripts/newsapp/attributes/admin.py
+++ Scripts/newsapp/attributes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import AuthNote, Note, Comment, Rating
 
 # Register your models here.
+admin.site.register(AuthNote)
+admin.site.register(Note)
+admin.site.register(Comment)
+admin.site.register(Rating)
--- Scripts/newsapp/attributes/views.py
+++ Scripts/newsapp/attributes/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.http.response import Http404
 from django.shortcuts import render
+
+from .models import Note
 
 # Create your views here.
 def authNotes(request):
     return HttpResponse('authNotes')
 
 def notes(request):
-    return HttpResponse('notes')
+    try:
+        notes = Note.objects.all()
+    except Note.DoesNotExist:
+        raise Http404("Poll does not exist")
+    return render(request, 'notes.html', {'notes': notes})
 
 def comments(request):
     return HttpResponse('comments')
