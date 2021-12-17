3e6644e5cd1eca3acdf56a0b63792b88fda8964c
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2021-12-17 03:44:05 -0700

    all topics and article topics displayed

--- Scripts/newsapp/articles/views.py
+++ Scripts/newsapp/articles/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import Article
+from .models import Article, Topic, NewsSource
 from django.http import HttpResponse
 
 # Create your views here.
 
 def article_list(request):
     articles = Article.objects.all()
-    return render(request, 'articles/article_list.html', {'articles': articles})
+    topics = Topic.objects.all()
+    context = {'articles': articles, 'topics': topics}
+    return render(request, 'articles/article_list.html', context)
 
 def article_details(request, slug):
     # return HttpResponse(slug)
