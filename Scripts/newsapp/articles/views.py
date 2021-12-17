0f38d5f7152cfefc18180924dddf79fecefdb1fa
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2021-12-16 21:59:16 -0700

    articles can be opened up and redirect to their own page

--- Scripts/newsapp/articles/views.py
+++ Scripts/newsapp/articles/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 # Create your views here.
 
 def article_list(request):
     articles = Article.objects.all()
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_details(request, slug):
+    # return HttpResponse(slug)
+    article = Article.objects.get(slug = slug)
+    return render(request, 'articles/article_details.html', {'article': article})
