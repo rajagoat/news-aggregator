52e352ceb6d7c92df80d0c5a80ad820b87df9365
noelfranthomas <noelfranthomas@gmail.com>
2021-12-17 06:53:36 -0700

    Create note button

--- Scripts/newsapp/articles/urls.py
+++ Scripts/newsapp/articles/urls.py
@@ -6,6 +6,7 @@ app_name = 'articles'
 urlpatterns = [
     path('', views.article_list, name='list'),
     path('note_create/', views.note_create, name='note_create'), # TEST
+    path('note_delete/', views.note_create, name='note_delete'), # TEST
     path('comment_create/', views.comment_create, name='comment_create'), # TEST
     path('rating_create/', views.rating_create, name='rating_create'), # TEST
     path('<slug:slug>/', views.article_details, name='details'),
