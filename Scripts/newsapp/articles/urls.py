8f3ced04f58be2159ebaa1a945bb61f65709364c
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2021-12-17 05:55:09 -0700

    Search functionality implemented

--- Scripts/newsapp/articles/urls.py
+++ Scripts/newsapp/articles/urls.py
@@ -5,5 +5,6 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.article_list, name='list'),
-    path('<slug:slug>/', views.article_details, name='details')
+    path('<slug:slug>/', views.article_details, name='details'),
+    path('search_results', views.search_results, name='search_results')
 ]
--- Scripts/newsapp/articles/views.py
+++ Scripts/newsapp/articles/views.py
@@ -14,3 +14,13 @@ def article_details(request, slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug = slug)
     return render(request, 'articles/article_details.html', {'article': article})
+
+def search_results(request):
+    if request.method == "POST":
+        searched = request.POST['search']
+        results = Article.objects.filter(name__icontains=searched)
+        articles = Article.objects.all()
+        context = {'articles': articles, 'searched': searched, 'results': results}
+        return render(request, 'articles/search_results.html', context)
+    else:
+        return render(request, 'articles/search_results.html', {})
